d8862229462b1a0eade092b875f12b89978ed516
Dogukan Olgun <dogukanolgun20000@gmail.com>
2022-02-27 10:57:29 +0200

    tutorial-5

--- Assets/Scripts/GridElement.cs
+++ Assets/Scripts/GridElement.cs
@@ -17,13 +17,37 @@ public class GridElement : MonoBehaviour
     private Coord coord;
     private Collider col;
     private Renderer rend;
+    public CornerElement[] corners = new CornerElement[8];
 
     public void Initialize(int setX, int setY, int setZ)
     {
+        int width = LevelGenerator.instance.width;
+        int height = LevelGenerator.instance.height;
         coord = new Coord(setX, setY, setZ);
         this.name = "GE_" + this.coord.x + "_" + this.coord.y + "_" + this.coord.z;
         this.col = this.GetComponent<Collider>();
         this.rend = this.GetComponent<Renderer>();
+
+        // setting corner elements
+        corners[0] = LevelGenerator.instance.cornerElements[coord.x + (width + 1) * (coord.z + (width + 1) * coord.y)];
+        corners[1] = LevelGenerator.instance.cornerElements[coord.x + 1 + (width + 1) * (coord.z + (width + 1) * coord.y)];
+        corners[2] = LevelGenerator.instance.cornerElements[coord.x + (width + 1) * (coord.z + 1 + (width + 1) * coord.y)];
+        corners[3] = LevelGenerator.instance.cornerElements[coord.x + 1 + (width + 1) * (coord.z + 1 + (width + 1) * coord.y)];
+        corners[4] = LevelGenerator.instance.cornerElements[coord.x + (width + 1) * (coord.z + (width + 1) * (coord.y + 1))];
+        corners[5] = LevelGenerator.instance.cornerElements[coord.x + 1 + (width + 1) * (coord.z + (width + 1) * (coord.y + 1))];
+        corners[6] = LevelGenerator.instance.cornerElements[coord.x + (width + 1) * (coord.z + 1 + (width + 1) * (coord.y + 1))];
+        corners[7] = LevelGenerator.instance.cornerElements[coord.x + 1 + (width + 1) * (coord.z + 1 + (width + 1) * (coord.y + 1))];
+
+        // positioning corner elements
+        corners[0].SetPosition(col.bounds.min.x, col.bounds.min.y, col.bounds.min.z);
+        corners[1].SetPosition(col.bounds.max.x, col.bounds.min.y, col.bounds.min.z);
+        corners[2].SetPosition(col.bounds.min.x, col.bounds.min.y, col.bounds.max.z);
+        corners[3].SetPosition(col.bounds.max.x, col.bounds.min.y, col.bounds.max.z);
+        corners[4].SetPosition(col.bounds.min.x, col.bounds.max.y, col.bounds.min.z);
+        corners[5].SetPosition(col.bounds.max.x, col.bounds.max.y, col.bounds.min.z);
+        corners[6].SetPosition(col.bounds.min.x, col.bounds.max.y, col.bounds.max.z);
+        corners[7].SetPosition(col.bounds.max.x, col.bounds.max.y, col.bounds.max.z);
+
     }
 
     public Coord GetCoord()
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -8,15 +8,32 @@ public class LevelGenerator : MonoBehaviour
     public int width = 5;
     public int height = 10;
     public GridElement GridElement;
+    public CornerElement cornerElement;
     public GridElement[] GridElements;
+    public CornerElement[] cornerElements;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
         instance = this;
 
         GridElements = new GridElement[width * width * height];
+        cornerElements = new CornerElement[(width + 1) * (width + 1) * (height + 1)];
+
+        for (int y = 0; y < height + 1; y++)
+        {
+            for (int x = 0; x < width + 1; x++)
+            {
+                for (int z = 0; z < width + 1; z++)
+                {
+                    CornerElement cornerElementInsance
+                            = Instantiate(cornerElement, Vector3.zero, Quaternion.identity, this.transform);
+                    cornerElementInsance.Initialize(x, y, z);
+                    cornerElements[x + (width + 1) * (z + (width + 1) * y)] = cornerElementInsance;
+                }
+            }
+        }
 
         for (int y = 0; y < height; y++)
         {
